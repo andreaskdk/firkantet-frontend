8fdde874d79e07c6cbe086bbd647b4cc775482aa
Andreas Koch <andreas@andreas-koch.dk>
2020-08-01 20:04:31 +0200

    added simple jest test

---
+++ __test__/load_firkant_test.spec.js
@@ -0,0 +1,13 @@
+const firkant=require("../index.js")
+
+describe("Load firkant", () => {
+    test("Should be able to see version number of firkant", () => {
+        const v=firkant.version;
+  
+        let re=/[0-9]+.[0-9]+.[0-9]+/
+        let m=re.test(v);
+  
+        expect(m).toEqual(true);
+  
+    });
+  });
