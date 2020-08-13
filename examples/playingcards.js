60b591b2d1c7e8c58c589ece401eaae7ebf4e8e1
Andreas Koch <andreas@andreas-koch.dk>
2020-08-13 16:54:58 +0200

    basic working example

---
+++ examples/playingcards.js
@@ -0,0 +1,5 @@
+const playingcards={
+    sourcename: "playing_cards_suites",
+    columns: ["Suit", "Colour", "Icon"],
+    data: [["Clubs", "Black", "♣"], ["Spades", "Black", "♠"], ["Hearts", "Red", "♥"], ["Diamonds", "Red", "♦"]]
+};
