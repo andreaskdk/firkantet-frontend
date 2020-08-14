3aff423465b34563488d8968ff51bea562ffbeee
Andreas Koch <andreas@andreas-koch.dk>
2020-08-14 23:37:19 +0200

    Rollup build

--- __test__/load_firkant_test.spec.js
+++ __test__/load_firkant_test.spec.js
@@ -1,4 +1,4 @@
-const firkant = require("../index.js")
+var firkant = require("../src")
 
 describe("Load firkant", () => {
     test("Should be able to see version number of firkant", () => {
@@ -10,4 +10,18 @@ describe("Load firkant", () => {
         expect(m).toEqual(true);
 
     });
+});
+
+describe("Get d3 version", () => {
+    test("Should be able to see version number of d3", () => {
+        const v = firkant.getD3Version();
+
+        let re = /[0-9]+.[0-9]+.[0-9]+/
+        let m = re.test(v);
+
+        expect(m).toEqual(true);
+        expect(v).toEqual("5.16.0");
+
+    });
+
 });
